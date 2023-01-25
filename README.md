c80c49994b031e84a59797836cbb7b075de116b8
madortill <mador.till@gmail.com>
2023-01-25 15:44:10 +0200

    starting project❤

---
+++ README.md
@@ -0,0 +1,15 @@
+## Boilerplate Instructions - read before start coding
+This template should help get you started developing with Vue 2 in Vite.  
+Thid is a boilerplate created by the command "npm init@vue2".
+
+**IMPORTANT! Before first upload - change <REPO_NAME> in `./vite.config.js` to your repository name.**  
+
+*Project Setup - before the first run:* run npm install  
+*run for development purposes:* npm run dev
+
+**Tips I found while working:🤩**
+- Start every static assets' url with "@" (equivalent to src directory), so the assets will appear as they should in final site.  
+- In order to register an event listener to native event (like "click") on a component, add native after the event name like `@click.native = "function"`  
+     
+     
+   *Tamar Stupp 2022*
---
+++ initGit.ps1
@@ -0,0 +1,39 @@
+  # set url parameter
+  Param ($repoURL = $(throw "repository url parameter is required."))
+  $repoName = ((($repoURL -split "/")[4]) -split ("\."))[0]  
+  write-output "Your repository name:  $repoName"
+
+# catch git errors function
+  function Invoke-Utility {
+    $exe, $argsForExe = $Args
+    # Workaround: Prevents 2> redirections applied to calls to this function
+    #             from accidentally triggering a terminating error.
+    #             See bug report at https://github.com/PowerShell/PowerShell/issues/4002
+    $ErrorActionPreference = 'Continue'
+    try { & $exe $argsForExe } catch { Throw } # catch is triggered ONLY if $exe can't be found, never for errors reported by $exe itself
+    if ($LASTEXITCODE) { Throw "$exe indicated failure (exit code $LASTEXITCODE; full command: $Args)." }
+  }
+  Set-Alias iu Invoke-Utility
+
+# check if connected to remote repo
+if ((git remote -v) -ne $null) {
+  Throw "This repository already exists in github! (has a remote)"
+}
+
+# replace <REPO_NAME> value in vite.config.js
+  $data = Get-Content ".\vite.config.js"
+  $data = $data.Replace("<REPO_NAME>", "$repoName")
+  $data | Out-File -encoding ASCII ".\vite.config.js"
+
+  npm i
+  iu git init
+  iu git remote add origin $repoURL
+  iu git add -A
+  iu git checkout -B master
+  iu git commit -m 'first' 
+  iu git push -u origin master
+  write-output ""
+  write-output "Repositry initalized"
+
+
+
