eea2d7767728190a368482b3b89f6ee8cd2eca8a
madortill <mador.till@gmail.com>
2023-03-12 14:34:29 +0200

    change heights

--- src/App.vue
+++ src/App.vue
@@ -47,11 +47,14 @@ export default {
 
 body {
   margin: 0;
-  height: 60%;
-  width: 100%;
+  height: 80vh;
+  width: 100vw;
+  overflow: hidden;
 }
 
 #app {
+  height: 100%;
+  width: 100%;
   background-image: url("@/assets/images/background.png");
   background-repeat: no-repeat;
   background-size: cover;
